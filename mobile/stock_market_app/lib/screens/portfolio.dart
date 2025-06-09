91828afee0c0ee627d28203482a642793231d57d
Wheesk <sardorsardoreodb@gmail.com>
2025-06-09 07:46:24 +0200

    testing

--- mobile/stock_market_app/lib/screens/portfolio.dart
+++ mobile/stock_market_app/lib/screens/portfolio.dart
@@ -39,7 +39,7 @@ class _PortfolioScreenState extends State<PortfolioScreen> {
       'TSLA': Colors.red,
       'NFLX': Colors.purple,
       'BTC': Colors.orange,
-      'ETH': Colors.green,
+      'ETH': Colors.black,
       'SOL': Colors.teal,
     };
     return colorMap[symbol] ?? Colors.grey;
